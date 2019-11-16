a4a973b3d882e8a37d1929a3eae48494467217c2
Sinan <me@wunnle.com>
2019-11-16 22:02:12 +0300

    more fine tuning

--- src/ShufflingCards/index.js
+++ src/ShufflingCards/index.js
@@ -5,43 +5,6 @@ const trans = (x, y, z) => ({
   transform: `rotateY(-20deg) translateX(${x}px) translateY(${y}px) translateZ(${z}px)`
 });
 
-const Card = ({ order, img, arr, shouldFlip, i }) => {
-
-
-
-
-  console.log({ img })
-
-  const spring = useSpring({
-    to: async next => {
-      if (order === 0) {
-        const goUp = -450 - (arr.length - 3) * 30;
-        const o = arr.length;
-        if (shouldFlip.current) {
-          await next(trans(o * -30, goUp, o * 10));
-        }
-        await next(trans(o * -30, o * +30, o * 10));
-      }
-      if (order > 0) {
-        const o = order;
-        await next(trans(-28 * o, 28 * o, 10.4 * o));
-        await next(trans(-30 * o, 30 * o, 10 * o));
-      }
-    },
-    from: trans(50, -50, 0)
-  });
-
-  return (
-    <animated.div style={spring}>
-      <div
-        className="container1 container"
-        style={{ backgroundImage: `url(${img})` }}>
-        index: {i}
-      </div>
-    </animated.div>
-  )
-}
-
 
 const ShufflingCards = ({ arr, targetImg = 0 }) => {
   const [orders, setOrders] = useState(arr.map((_, i) => i));
@@ -75,11 +38,49 @@ const ShufflingCards = ({ arr, targetImg = 0 }) => {
 
   return (
     <div className="wrapper">
-      {arr.map((img, i) => (
+      {arr.map((_img, i) => (
         <Card i={i} key={`img${i}`} img={arr[i]} order={orders[i]} arr={arr} shouldFlip={shouldFlip} />
       ))}
     </div>
   );
+}
+
+const Card = ({ order, img, arr, shouldFlip, i }) => {
+
+  const spring = useSpring({
+    config: {
+      mass: 0.5,
+      tension: 150,
+      friction: 26
+    },
+    to: async next => {
+      if (order === 0) {
+        const goUp = -800 - (arr.length - 3) * 30;
+        const o = arr.length;
+        if (shouldFlip.current) {
+          console.log(o)
+          await next(trans(o * -30, goUp, o * 10));
+        }
+        await next(trans(o * -30, o * 30, o * 10));
+      }
+      if (order > 0) {
+        const o = order;
+        await next(trans(-30 * o, 30 * o, 10.9 * o));
+        await next(trans(-30 * o, 30 * o, 10 * o));
+      }
+    },
+    from: trans(50, -50, 0)
+  });
+
+  return (
+    <animated.div style={spring}>
+      <div
+        className="container1 container"
+        style={{ backgroundImage: `url(${img})` }}>
+        index: {i}
+      </div>
+    </animated.div>
+  )
 }
 
 export default ShufflingCards
